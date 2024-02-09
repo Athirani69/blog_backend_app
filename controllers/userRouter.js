0b23e18d407e54bdbe380a7481993bf65229ad18
Athirani69 <Athirani69@gmail.com>
2024-02-09 10:15:05 +0530

    new one created

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,10 +1,15 @@
 const express = require("express")
 
 const userRoute=express.Router()
+const usrMdl=require("../models/usermodel")
 
-
-Router.post("/add",(res,res)=>{
-    res.send("hy")
+Router.post("/add",async(res,res)=>{
+    let data=req.body
+    let usrobj=new usrMdl(data)
+    let result=await usrobj.save()
+    res.json({
+        status:"success"
+    })
 })
 
 
