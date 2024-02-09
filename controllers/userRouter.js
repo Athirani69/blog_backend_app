7abf4720700193caae6881c997229255a0103e9a
Athirani69 <Athirani69@gmail.com>
2024-02-09 11:28:47 +0530

    encryption performed

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -2,15 +2,36 @@ const express = require("express")
 
 const userRoute=express.Router()
 const usrMdl=require("../models/usermodel")
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(password)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
+
 
 userRoute.post("/add",async(req,res)=>{
-    let data=req.body
-    let usrobj=new usrMdl(data)
-    let result=await usrobj.save()
-    res.json({
-        status:"success"
-    })
-})
+
+    let{data}={"data":req.body}
+    let pass=req.body.pass
+    hashPasswordGenerator(pass).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.pass=hashedPassword
+            console.log(data)
+            let usrobj=new usrMdl(data)
+            let result=usrobj.save()
+            res.json({
+                status:"success"
+            })
+        })
+        }
+    )
+    
+
+    //let data=req.body
+    
+    
 
 
 
